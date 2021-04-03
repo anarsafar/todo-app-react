a2d6a64d2bccfc9c4487f3bf20ed2055331f8d9d
Anar Səfərov <1anarseferov@gmail.com>
2021-04-03 14:53:25 +0400

    added header

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,11 @@
+import Header from "./Header";
+
 function Container() {
-  return <div>Container</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Container;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import bgLight from "../images/bg-desktop-light.jpg";
+// import bgDark from "../images/bg-desktop-dark.jpg";
+
+function Header() {
+  return (
+    <header>
+      <img src={bgLight} alt="" />
+    </header>
+  );
+}
+
+export default Header;
