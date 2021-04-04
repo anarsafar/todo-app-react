153578602b3a1715247880f3bd999b5e6b360f1f
Anar Səfərov <1anarseferov@gmail.com>
2021-04-04 18:08:18 +0400

    added react hooks for saving input data

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 function AddTodo() {
+  const [todo, setTodo] = useState([]);
+  const [todoItem, setTodoItem] = useState("");
+
+  const handleSubmit = (e) => {
+    setTodo((todo) => [...todo, todoItem]);
+    setTodoItem("");
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setTodoItem(value);
+  };
+
+  console.log(todo);
   return (
-    <form className="flex">
+    <form className="flex" onSubmit={handleSubmit}>
       <button></button>
-      <input type="text" placeholder="Create new todo..." />
+      <input
+        type="text"
+        placeholder="Create a new todo..."
+        value={todoItem}
+        onChange={handleChange}
+      />
     </form>
   );
 }
