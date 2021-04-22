179094587d7aa8c12d2a08d94b09f378a0c13a13
Anar Səfərov <1anarseferov@gmail.com>
2021-04-22 16:44:07 +0400

    presisted dark mode with local storage

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoContainer from "./TodoContainer";
 import bgDesktopLight from "../images/bg-desktop-light.jpg";
 import bgDesktopDark from "../images/bg-desktop-dark.jpg";
+import LS from "local-storage";
 
 function Container() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const isDark = LS.get("darkModeLS");
+    return isDark !== null ? isDark : false;
+  });
+  useEffect(() => {
+    LS.set("darkModeLS", darkMode);
+  }, [darkMode]);
 
   const handleDarkMode = () => {
     setDarkMode((prevState) => !prevState);
-    console.log(darkMode);
+    console.log("click: " + darkMode);
   };
+
   if (darkMode) {
     document.body.style.backgroundColor = `hsl(235,21%,11%)`;
   } else {
     document.body.style.backgroundColor = `#fff`;
   }
+
+  console.log("app: " + darkMode);
   return (
     <div>
       <header>
